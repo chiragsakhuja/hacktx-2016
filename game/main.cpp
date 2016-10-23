a2d91ccb6ba66e572f64b03fdd70d3e5dd625f36
Nikhil Dixit <nikhildixit@utexas.edu>
2016-10-23 10:56:16 -0500

    w0t2

--- game/main.cpp
+++ game/main.cpp
@@ -265,11 +265,12 @@ int Game::render(int frame)
     glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(1.0f));
     glm::mat4 rotate = glm::rotate(glm::mat4(1.0f), 0.0f, glm::vec3(0.0f, 1.0f, 0.0f));
     glm::mat4 translate = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f));
-
+    
+    glm::mat4 view;
     if (my_id == 0)
-        glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        view = glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.0f, 1.0f, 0.0f));
     else
-        glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 0.0f, -10.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        view = glm::lookAt(glm::vec3(0.0f, 0.0f, -10.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 
     glm::mat4 world, wvp;
     glm::mat4 perspective = glm::perspective(glm::radians(30.0f), (float) WIDTH / (float) HEIGHT, 0.1f, 100.0f);
