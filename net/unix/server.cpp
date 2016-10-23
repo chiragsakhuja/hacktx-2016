27e4f533195b3c016027ab91d70de33db8d3389c
Alex Hsu <alex.hsu@utexas.edu>
2016-10-23 11:56:19 -0500

    collision logic fixes

--- net/unix/server.cpp
+++ net/unix/server.cpp
@@ -35,8 +35,8 @@ public:
 
     void start()
     {
-        game.ball.pos.z = game.start_player * 10;
-        game.ball.vec.z = (-2 * game.start_player);
+        game.ball.pos.z = 5.0f;
+        game.ball.vec.z = game.start_player ? 2.0f : -2.0f;
 
         std::ostringstream out_data_str;
         ptree out_data_tree;
@@ -53,20 +53,22 @@ public:
     void update()
     {
         game_t::ball_t& ball = game.ball;
+
         constexpr float delta = 1.0f/60;
         constexpr float ball_radius = 0.1f;
         constexpr float paddle_size = 0.6f;
-        constexpr float speed_up = 1.1f;
-        constexpr float acc_factor = 0.1f;
+        constexpr float speed_up = 1.05f;
+        constexpr float acc_factor = 0.01f;
         constexpr float aspect_ratio = 720.0f / 1280;
+
         while (true) {
             std::this_thread::sleep_for(std::chrono::microseconds((uint64_t) (delta * 1000000)));
 
             ball.pos.x += ball.vec.x * delta;
             ball.pos.y += ball.vec.y * delta;
             ball.pos.z += ball.vec.z * delta;
-            ball.vec.x += ball.acc.x * delta;
-            ball.vec.y += ball.acc.y * delta;
+            //ball.vec.x += ball.acc.x * delta;
+            //ball.vec.y += ball.acc.y * delta;
 
             if ((ball.pos.x + ball_radius) > 1.0f) {
                 ball.pos.x = 1.0f;
@@ -84,32 +86,36 @@ public:
             }
 
             if ((ball.pos.z - ball_radius)< 0.0f) {
-                if (ball.pos.x > (game.player[0].pos.x - paddle_size / 2) &&
-                    ball.pos.x < (game.player[0].pos.x + paddle_size / 2) &&
-                    ball.pos.y > (game.player[0].pos.y - paddle_size / 2) &&
-                    ball.pos.y < (game.player[0].pos.y + paddle_size / 2)) {
+                if (ball.pos.x > (game.player[0].pos.x - paddle_size / 2.0f) &&
+                    ball.pos.x < (game.player[0].pos.x + paddle_size / 2.0f) &&
+                    ball.pos.y > (game.player[0].pos.y - paddle_size / 2.0f) &&
+                    ball.pos.y < (game.player[0].pos.y + paddle_size / 2.0f)) {
+                    ball.vec.x += ((rand() % 20) - 10) * 0.01f;
+                    ball.vec.y += ((rand() % 20) - 10) * 0.01f;
                     ball.vec.z *= -speed_up;
                     ball.acc.x = game.player[0].vec.x * acc_factor;
                     ball.acc.y = game.player[0].vec.y * acc_factor;
                 } else {
                     game.player[1].score += 1;
                     game.start_player = !game.start_player;
-                    game.ball.pos.z = game.start_player * 10;
-                    game.ball.vec.z = (-2 * game.start_player);
+                    game.ball.pos.z = 5.0f;
+                    game.ball.vec.z = game.start_player ? 2.0f : -2.0f;
                 }
             } else if ((ball.pos.z + ball_radius) > 10.0f) {
-                if (ball.pos.x > (game.player[1].pos.x - paddle_size / 2) &&
-                    ball.pos.x < (game.player[1].pos.x + paddle_size / 2) &&
-                    ball.pos.y > (game.player[1].pos.y - paddle_size / 2) &&
-                    ball.pos.y < (game.player[1].pos.y + paddle_size / 2)) {
+                if (ball.pos.x > (game.player[1].pos.x - paddle_size / 2.0f) &&
+                    ball.pos.x < (game.player[1].pos.x + paddle_size / 2.0f) &&
+                    ball.pos.y > (game.player[1].pos.y - paddle_size / 2.0f) &&
+                    ball.pos.y < (game.player[1].pos.y + paddle_size / 2.0f)) {
+                    ball.vec.x += ((rand() % 20) - 10) * 0.01f;
+                    ball.vec.y += ((rand() % 20) - 10) * 0.01f;
                     ball.vec.z *= -speed_up;
                     ball.acc.x = game.player[1].vec.x * acc_factor;
                     ball.acc.y = game.player[1].vec.y * acc_factor;
                 } else {
                     game.player[0].score += 1;
                     game.start_player = !game.start_player;
-                    game.ball.pos.z = game.start_player * 10;
-                    game.ball.vec.z = (-2 * game.start_player);
+                    game.ball.pos.z = 5.0f;
+                    game.ball.vec.z = game.start_player ? 2.0f : -2.0f;
                 }
             }
 
@@ -168,6 +174,7 @@ int main(int argc, char** argv)
                         game.player[id].joined[0] = true;
                         game.player[id].addr[0] = packet.addr;
                         //cout << "player 0 phone @ " << game.player[id].addr[0].ip << ":" << game.player[id].addr[0].port << endl;
+                        cout << "phone " << id << " connected" << endl;
 
                         std::ostringstream out_data_str;
                         ptree out_data_tree;
@@ -186,6 +193,7 @@ int main(int argc, char** argv)
                         game.player[id].joined[1] = true;
                         game.player[id].addr[1] = packet.addr;
                         //cout << "player 0 laptop @ " << game.player[id].addr[1].ip << ":" << game.player[id].addr[1].port << endl;
+                        cout << "laptop " << id << " connected" << endl;
 
                         std::ostringstream out_data_str;
                         ptree out_data_tree;
@@ -199,7 +207,28 @@ int main(int argc, char** argv)
                     }
                 }
             } else if (game.status == game_t::status_enum::ready) {
-                if (req == "start") {
+                if (req == "update") {
+                    string px = in_data_tree.get<string>("px");
+                    string py = in_data_tree.get<string>("py");
+                    string vx = in_data_tree.get<string>("vx");
+                    string vy = in_data_tree.get<string>("vy");
+                    int id = std::atoi(in_data_tree.get<string>("id").c_str());
+
+                    game.player[id].pos.x = std::atoi(px.c_str());
+                    game.player[id].pos.y = std::atoi(py.c_str());
+                    game.player[id].vec.x = std::atoi(vx.c_str());
+                    game.player[id].vec.y = std::atoi(vy.c_str());
+
+                    std::ostringstream out_data_str;
+                    ptree out_data_tree;
+                    out_data_tree.put("obj", "paddle");
+                    out_data_tree.put("id", id);
+                    out_data_tree.put("px", px);
+                    out_data_tree.put("py", py);
+                    write_json(out_data_str, out_data_tree);
+                    server.send_all(out_data_str.str());
+                } else if (req == "start") {
+                    cout << "start" << endl;
                     bp.start();
 
                     game.status = game_t::status_enum::play;
