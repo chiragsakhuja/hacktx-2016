4073719071d84712897fd02d95749b1ad8f892cb
Alex Hsu <alex.hsu@utexas.edu>
2016-10-23 02:12:18 -0500

    build game constructs

--- net/unix/net.h
+++ net/unix/net.h
@@ -3,12 +3,74 @@
 #define SERVER_ADDR a-dev.me
 #define SERVER_PORT 1221
 
-class server
+struct game_t
+{
+    enum status_enum {
+        idle,
+        play
+    };
+
+    struct player_t
+    {
+        struct pos_t
+        {
+            float x = 0.0f;
+            float y = 0.0f;
+        };
+
+        struct vec_t
+        {
+            float x = 0.0f;
+            float y = 0.0f;
+        };
+
+        sockaddr_in addr[2];
+        bool joined[0];
+        pos_t pos;
+        vec_t vec;
+    };
+
+    struct ball_t
+    {
+        struct vec_t
+        {
+            float x = 0.0f;
+            float y = 0.0f;
+            float z = 0.0f;
+        };
+
+        struct acc_t
+        {
+            float x = 0.0f;
+            float y = 0.0f;
+        };
+
+        vec_t vec;
+        acc_t acc;
+    };
+
+    uint8_t status = status_enum::idle;
+    player_t player[2];
+    ball_t ball;
+};
+
+
+struct packet_t
+{
+    sockaddr_in addr;
+    string data;
+};
+
+std::vector<packet_t> packet_queue;
+std::mutex packet_queue_mutex;
+
+
+class server_t
 {
 public:
-    server()
+    server_t()
     {
-        socket_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+        socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
         if (socket_fd < 0) {
             throw std::runtime_error("cannot open socket_fd");
         }
@@ -24,10 +86,10 @@ public:
             throw std::runtime_error("cannot bind socket");
         }
 
-        t = std::thread{ &server::transcieve, this };
+        t = std::thread{ &server_t::transcieve, this };
     } 
 
-    ~server()
+    ~server_t()
     {
         t.join();
         close(socket_fd);
@@ -35,17 +97,41 @@ public:
 
     void transcieve()
     {
-        uint8_t msg[1024];
+        char msg[1024];
+        sockaddr_in other_addr;
         socklen_t other_addr_len;
         int msg_size;
         while (true) {
+            other_addr_len = sizeof(other_addr);
             msg_size = recvfrom(socket_fd, msg, 1023, 0, (struct sockaddr *) &other_addr, &other_addr_len);
             if (msg_size < 0) {
                 cout << strerror(errno) << endl;
                 throw std::runtime_error("failed to recieve data");
             }
             msg[msg_size] = '\0';
+            cout << "> " << inet_ntoa(other_addr.sin_addr) << ":" << ntohs(other_addr.sin_port) << endl;
             cout << msg << endl;
+            {
+                std::lock_guard<std::mutex> lock{ packet_queue_mutex };
+                packet_queue.push_back(
+                    packet_t {
+                        other_addr,
+                        string{ msg }
+                    }
+                );
+            }
+        }
+
+    }
+
+    void send(sockaddr_in addr, string data)
+    {
+        socklen_t addr_len = sizeof(addr);
+
+        int msg_size = sendto(socket_fd, data.c_str(), data.length(), 0, (struct sockaddr *) &addr, addr_len);
+        if (msg_size < 0) {
+            cout << strerror(errno) << endl;
+            throw std::runtime_error("failed to send data");
         }
     }
 
@@ -53,5 +139,4 @@ private:
     std::thread t;
     int socket_fd;
     sockaddr_in addr;
-    sockaddr_in other_addr;
 };
--- net/unix/server.cpp
+++ net/unix/server.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <thread>
+#include <mutex>
 #include <cstring>
 #include <cerrno>
 #include <strings.h>
@@ -8,16 +9,91 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
 
 using std::cout;
 using std::endl;
+using std::string;
+
+using boost::property_tree::ptree;
+using boost::property_tree::read_json;
+using boost::property_tree::write_json;
 
 #include "net.h"
+
+
+game_t game;
+
 
 int main(int argc, char** argv)
 {
     try {
-        server s;
+        server_t server;
+
+        while (true) {
+            while (packet_queue.empty()) {}
+
+            packet_t packet;
+            {
+                std::lock_guard<std::mutex> lock{ packet_queue_mutex };
+                packet = packet_queue.front();
+                packet_queue.pop_back();
+            }
+            std::istringstream in_data_str{ packet.data };
+            ptree in_data_tree;
+            read_json(in_data_str, in_data_tree);
+
+            string req = in_data_tree.get<string>("req");
+            string dev = in_data_tree.get<string>("dev");
+            if (req == "connect") {
+                if (dev == "phone") {
+                    size_t id;
+                    if (!game.player[0].joined[0])
+                        id = 0;
+                    else if (!game.player[1].joined[0])
+                        id = 1;
+                    else
+                        throw std::runtime_error("too many phones");
+                    game.player[id].addr[0] = packet.addr;
+                    //cout << "player 0 phone @ " << game.player[id].addr[0].ip << ":" << game.player[id].addr[0].port << endl;
+
+                    std::ostringstream out_data_str;
+                    ptree out_data_tree;
+                    out_data_tree.put("id", id);
+                    write_json(out_data_str, out_data_tree);
+                    server.send(game.player[id].addr[0], out_data_str.str());
+                }
+                else if (dev == "laptop") {
+                    size_t id;
+                    if (!game.player[0].joined[1])
+                        id = 0;
+                    else if (!game.player[1].joined[1])
+                        id = 1;
+                    else
+                        throw std::runtime_error("too many laptops");
+                    game.player[id].addr[1] = packet.addr;
+                    //cout << "player 0 laptop @ " << game.player[id].addr[1].ip << ":" << game.player[id].addr[1].port << endl;
+
+                    std::ostringstream out_data_str;
+                    ptree out_data_tree;
+                    out_data_tree.put("id", id);
+                    write_json(out_data_str, out_data_tree);
+                    server.send(game.player[id].addr[1], out_data_str.str());
+                }
+            } else if (req == "update") {
+                string px = in_data_tree.get<string>("px");
+                string py = in_data_tree.get<string>("py");
+
+                std::ostringstream out_data_str;
+                ptree out_data_tree;
+                out_data_tree.put("px", px);
+                out_data_tree.put("py", py);
+                write_json(out_data_str, out_data_tree);
+                server.send(game.player[0].addr[1], out_data_str.str());
+            }
+        }
+
     } catch (std::runtime_error e) {
         cout << "error: " << e.what() << endl;
     }
